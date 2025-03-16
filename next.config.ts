dc53dc6984eec2335410f3a4d90bc18b1fc13730
alexandre.verdu <developer@alexandreverdu.dev>
2025-03-16 15:32:03 +0100

    deploy

--- next.config.ts
+++ next.config.ts
@@ -2,19 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: true, // No recomendado en producción
   },
-  env: {
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    NEXT_IGNORE_TS_ERRORS: process.env.NEXT_IGNORE_TS_ERRORS,
-    NEXT_PUBLIC_1215DSF1412: process.env.NEXT_PUBLIC_1215DSF1412,
-    NEXT_PUBLIC_1234ABC567: process.env.NEXT_PUBLIC_1234ABC567,
-    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
-    SUPABASE_URL: process.env.SUPABASE_URL,
-    SUPABASE_PDF_CONTAINER: process.env.SUPABASE_PDF_CONTAINER,
-    SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
-  },
+  reactStrictMode: true
 };
 
 export default nextConfig;
